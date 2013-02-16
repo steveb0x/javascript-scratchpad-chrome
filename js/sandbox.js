5554467aa415255984d70a085c62da6fe31c2841
Daniel Huigens <d.huigens@gmail.com>
2013-02-16 14:40:25 +0100

    Hide helper variables from execution context

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,37 +1,33 @@
 function setUpWorker() {
 	worker = new Worker(window.URL.createObjectURL(new Blob(["\
-var window = this;\
-var messages = [];\
-setInterval(function() {\
-	if(messages.length) {\
-		var msg;\
-		try {\
-			self.postMessage(msg = messages.shift());\
-		} catch(e) {\
-			if(msg.args) {\
-				msg.args = msg.args.map(function(elm) {\
-					return elm + '';\
-				});\
-				self.postMessage(msg);\
+(function() {\
+	var messages = [];\
+	setInterval(function() {\
+		if(messages.length) {\
+			var msg;\
+			try {\
+				self.postMessage(msg = messages.shift());\
+			} catch(e) {\
+				if(msg.args) {\
+					msg.args = msg.args.map(function(elm) {\
+						return elm + '';\
+					});\
+					self.postMessage(msg);\
+				}\
 			}\
 		}\
-	}\
-}, 100);\
-var getConsoleMethod = function(method) {\
-	return function() {\
-		messages.push({\
-			cmd: 'console',\
-			method: method,\
-			args: [].slice.call(arguments)\
-		});\
+	}, 100);\
+	var getConsoleMethod = function(method) {\
+		return function() {\
+			messages.push({\
+				cmd: 'console',\
+				method: method,\
+				args: [].slice.call(arguments)\
+			});\
+		};\
 	};\
-};\
-\
-var console = (function() {\
-	var time = getConsoleMethod('time'),\
-		timeEnd = getConsoleMethod('timeEnd'),\
-		times = {};\
-	return {\
+	var times = {};\
+	self.console = {\
 		log: getConsoleMethod('log'),\
 		debug: getConsoleMethod('debug'),\
 		time: function(name) {\
@@ -42,17 +38,16 @@ var console = (function() {\
 			console.debug(name + ': ' + (+new Date() - times[name]) + 'ms');\
 		}\
 	};\
-})();\
-\
-var geval = eval;\
-self.onmessage = function(event) {\
-	geval(event.data);\
-	setTimeout(function() {\
-		messages.push({\
-			cmd: 'done'\
+	self.onmessage = function(event) {\
+		self.eval(event.data);\
+		setTimeout(function() {\
+			messages.push({\
+				cmd: 'done'\
+			});\
 		});\
-	});\
-};"], {type: 'text/javascript'})));
+	};\
+})();\
+"], {type: 'text/javascript'})));
 	worker.addEventListener('message', function(event) {
 		source.postMessage(event.data, origin);
 	}, false);
